88812be161bceb533952ebaa7bf81af4bc328012
chrisfevo <cdasaro@fevo.com>
2024-01-29 09:44:27 -0500

    no message

--- test.zsh
+++ test.zsh
@@ -1,14 +1,12 @@
 #!/bin/zsh
 
 kubectl create namespace argocd
-helm repo add argo https://argoproj.github.io/argo-helm
+#helm repo add argo https://argoproj.github.io/argo-helm
 #kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 helm dependency build
-helm upgrade --install argocd . -n argocd
-$ kubectl port-forward svc/argo-cd-argocd-server 8080:443
-$ kubectl get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-7gE4Y-SeqmDsrw0n
-mLDFQSCe4lpm9LMJ
-
- helm template root-app/ | kubectl apply -f -
+helm upgrade --install argo-cd . -f values.yaml
+helm template root-app/ | kubectl apply -f -
+kubectl get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+kubectl port-forward svc/argo-cd-argocd-server 8085:80
+8WTgZw5OYPNnymFR
 
